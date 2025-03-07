ce24ad612139aeb2c57029e609a9eab1e15ed40d
NicolaBUZ <nicola.bianchini@buhlergroup.com>
2025-03-07 14:43:41 +0100

    gamehub responsive

--- script/script.js
+++ script/script.js
@@ -6,6 +6,5 @@ const content = document.getElementById("content");
     content.style.height = `${((window.innerHeight)/20)*19}px`
     content.style.width = `${((window.innerWidth)/20)*19}px`
 
-    content.style.marginLeft = `${(window.innerWidth - content.offsetWidth)/2}px`;
     content.style.marginTop = `${(window.innerHeight - content.offsetHeight)/2}px`;
 }
