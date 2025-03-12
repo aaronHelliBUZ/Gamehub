7b75435137642dbfd27ee5790933896490014a54
aaronHelliBUZ <aaron.hellmueller@buhlergroup.com>
2025-03-12 13:47:36 +0100

    Dynamische Äpfel

--- Snake/scripts/script.js
+++ Snake/scripts/script.js
@@ -1,217 +1,219 @@
-let richtung = ['A', 'A', 'A'];
-let aktuelleRichtung = 'A';
-let laenge = 3;
+function script(aepfel){
+    let richtung = ['A', 'A', 'A'];
+    let aktuelleRichtung = 'A';
+    let laenge = 3;
 
-document.addEventListener('keydown', function(event){
-    if(event.code === 'KeyW' || event.code === 'ArrowUp'){
-        aktuelleRichtung = 'W';
-    }else if(event.code === 'KeyD' || event.code === 'ArrowRight'){
-        aktuelleRichtung = 'D';
-    }else if(event.code === 'KeyS' || event.code === 'ArrowDown'){
-        aktuelleRichtung = 'S';
-    }else if(event.code === 'KeyA' || event.code === 'ArrowLeft'){
-        aktuelleRichtung = 'A';
-    }
-})
+    document.addEventListener('keydown', function(event){
+        if(event.code === 'KeyW' || event.code === 'ArrowUp'){
+            aktuelleRichtung = 'W';
+        }else if(event.code === 'KeyD' || event.code === 'ArrowRight'){
+            aktuelleRichtung = 'D';
+        }else if(event.code === 'KeyS' || event.code === 'ArrowDown'){
+            aktuelleRichtung = 'S';
+        }else if(event.code === 'KeyA' || event.code === 'ArrowLeft'){
+            aktuelleRichtung = 'A';
+        }
+    })
 
-let = schlangeElm = document.createElement('div');
-schlangeElm.id = 'Kopf';
-schlangeElm.style.position = "relative";
-schlangeElm.style.height = "100%";
-schlangeElm.style.width = "100%";
-
-let schlangeParent = document.getElementById('4_4');
-schlangeParent.classList.add('Kopf');
-schlangeParent.classList.add('Schlange');
-schlangeParent.appendChild(schlangeElm);
-
-for(let i = 1; i <= 2; i++){
-    schlangeElm = document.createElement('div');
+    let = schlangeElm = document.createElement('div');
+    schlangeElm.id = 'Kopf';
     schlangeElm.style.position = "relative";
     schlangeElm.style.height = "100%";
     schlangeElm.style.width = "100%";
 
-    schlangeParent = document.getElementById(`4_${4 + i}`)
-    schlangeParent.classList.add('Schlange')
-
-    if(i == 2){
-        schlangeElm.id = "Schwanz";
-        schlangeParent.classList.add('Schwanz');
-    }
-
+    let schlangeParent = document.getElementById('4_4');
+    schlangeParent.classList.add('Kopf');
+    schlangeParent.classList.add('Schlange');
     schlangeParent.appendChild(schlangeElm);
-}
 
-function randomApfel(){
-    let randY = parseInt(Math.random() * 9);
-    let randX = parseInt(Math.random() * 10);
+    for(let i = 1; i <= 2; i++){
+        schlangeElm = document.createElement('div');
+        schlangeElm.style.position = "relative";
+        schlangeElm.style.height = "100%";
+        schlangeElm.style.width = "100%";
 
-    let apfelElm = document.createElement('img');
-    apfelElm.src = "img/google-search-snake.png";
-    apfelElm.style.height = "100%";
-    apfelElm.style.width = "100%";
+        schlangeParent = document.getElementById(`4_${4 + i}`)
+        schlangeParent.classList.add('Schlange')
 
-    let randPosition = document.getElementById(`${randY}_${randX}`);
-    if(randPosition.classList.contains('Apfel') || randPosition.classList.contains('Schlange')){
-        randomApfel();
-    }else{
-        randPosition.classList.add('Apfel')
-        randPosition.appendChild(apfelElm);
+        if(i == 2){
+            schlangeElm.id = "Schwanz";
+            schlangeParent.classList.add('Schwanz');
+        }
+
+        schlangeParent.appendChild(schlangeElm);
     }
-}
 
-for(let i = 1; i <= 3; i++){
-    randomApfel();
-}
+    function randomApfel(){
+        let randY = parseInt(Math.random() * 9);
+        let randX = parseInt(Math.random() * 10);
 
-function bewegen(){
-    let kopfElms = document.getElementsByClassName('Kopf');
-    let kopfElm = kopfElms.item(0)
-    kopfElm.classList.remove('Kopf');
+        let apfelElm = document.createElement('img');
+        apfelElm.src = "img/google-search-snake.png";
+        apfelElm.style.height = "100%";
+        apfelElm.style.width = "100%";
 
-    let neuerKopfElm;
-    
-    let [y, x] = kopfElm.id.split('_')
-    x = parseInt(x);
-    y = parseInt(y);
-    
-    richtungLaenge = richtung.length;
-    console.log(richtungLaenge)
+        let randPosition = document.getElementById(`${randY}_${randX}`);
+        if(randPosition.classList.contains('Apfel') || randPosition.classList.contains('Schlange')){
+            randomApfel();
+        }else{
+            randPosition.classList.add('Apfel')
+            randPosition.appendChild(apfelElm);
+        }
+    }
 
-    document.getElementById('Kopf').remove();
+    for(let i = 1; i <= aepfel; i++){
+        randomApfel();
+    }
 
-    schlangeElm = document.createElement('div');
-    schlangeElm.style.position = "relative";
-    schlangeElm.style.height = "100%";
-    schlangeElm.style.width = "100%";
+    function bewegen(){
+        let kopfElms = document.getElementsByClassName('Kopf');
+        let kopfElm = kopfElms.item(0)
+        kopfElm.classList.remove('Kopf');
 
-    kopfElm.appendChild(schlangeElm);
+        let neuerKopfElm;
+        
+        let [y, x] = kopfElm.id.split('_')
+        x = parseInt(x);
+        y = parseInt(y);
+        
+        richtungLaenge = richtung.length;
+        console.log(richtungLaenge)
 
-    
-    switch(aktuelleRichtung){
-        case 'W':
-            console.log(richtung[richtungLaenge - 1])
-            if(richtung[richtungLaenge - 1] == 'S'){
-                neuerKopfElm = document.getElementById(`${y + 1}_${x}`);
-                richtung.push('S');
-                console.log(richtung);
-                break;
-            }
-            neuerKopfElm = document.getElementById(`${y - 1}_${x}`);
-            richtung.push('W');
-            console.log(richtung);
-            break;
-        case 'S':
-            console.log(richtung[richtungLaenge - 1])
-            if(richtung[richtungLaenge - 1] == 'W'){
+        document.getElementById('Kopf').remove();
+
+        schlangeElm = document.createElement('div');
+        schlangeElm.style.position = "relative";
+        schlangeElm.style.height = "100%";
+        schlangeElm.style.width = "100%";
+
+        kopfElm.appendChild(schlangeElm);
+
+        
+        switch(aktuelleRichtung){
+            case 'W':
+                console.log(richtung[richtungLaenge - 1])
+                if(richtung[richtungLaenge - 1] == 'S'){
+                    neuerKopfElm = document.getElementById(`${y + 1}_${x}`);
+                    richtung.push('S');
+                    console.log(richtung);
+                    break;
+                }
                 neuerKopfElm = document.getElementById(`${y - 1}_${x}`);
                 richtung.push('W');
                 console.log(richtung);
                 break;
-            }
-            neuerKopfElm = document.getElementById(`${y + 1}_${x}`);
-            richtung.push('S');
-            console.log(richtung);
-            break;
-        case 'A':
-            console.log(richtung[richtungLaenge - 1])
-            if(richtung[richtungLaenge - 1] == 'D'){
-                neuerKopfElm = document.getElementById(`${y}_${x + 1}`);
-                richtung.push('D');
+            case 'S':
+                console.log(richtung[richtungLaenge - 1])
+                if(richtung[richtungLaenge - 1] == 'W'){
+                    neuerKopfElm = document.getElementById(`${y - 1}_${x}`);
+                    richtung.push('W');
+                    console.log(richtung);
+                    break;
+                }
+                neuerKopfElm = document.getElementById(`${y + 1}_${x}`);
+                richtung.push('S');
                 console.log(richtung);
                 break;
-            }
-            neuerKopfElm = document.getElementById(`${y}_${x - 1}`);
-            richtung.push('A');
-            console.log(richtung);
-            break;
-        case 'D':
-            console.log(richtung[richtungLaenge - 1])
-            if(richtung[richtungLaenge - 1] == 'A'){
+            case 'A':
+                console.log(richtung[richtungLaenge - 1])
+                if(richtung[richtungLaenge - 1] == 'D'){
+                    neuerKopfElm = document.getElementById(`${y}_${x + 1}`);
+                    richtung.push('D');
+                    console.log(richtung);
+                    break;
+                }
                 neuerKopfElm = document.getElementById(`${y}_${x - 1}`);
                 richtung.push('A');
                 console.log(richtung);
                 break;
-            }
-            neuerKopfElm = document.getElementById(`${y}_${x + 1}`);
-            richtung.push('D');
-            console.log(richtung);
-            break;
-    }
-    if(x == 0 && richtung[richtungLaenge] == 'A' || x == 9 && richtung[richtungLaenge] == 'D' || y == 0 && richtung[richtungLaenge] == 'W' || y == 8 && richtung[richtungLaenge] == 'S'){
-        sterben();
-    }
-    if(neuerKopfElm.classList.contains('Apfel')){
-        laenge++;
-
-        if(laenge == 90){
-            alert('win');
-            location.reload;
-        }
-
-        schlangeElm = document.createElement('div');
-        schlangeElm.id = 'Kopf';
-        schlangeElm.style.position = "relative";
-        schlangeElm.style.height = "100%";
-        schlangeElm.style.width = "100%";
-        
-        neuerKopfElm.removeChild(neuerKopfElm.firstChild);
-        neuerKopfElm.classList.add('Kopf');
-        neuerKopfElm.classList.add('Schlange');
-        neuerKopfElm.classList.remove('Apfel');
-        if(laenge < 88){
-            randomApfel();
-        }
-        neuerKopfElm.appendChild(schlangeElm);
-    }else if(neuerKopfElm.classList.contains('Schlange') && !(neuerKopfElm.classList.contains('Schwanz'))){
-        sterben();
-    }else{
-        schlangeElm = document.createElement('div');
-        schlangeElm.id = 'Kopf';
-        schlangeElm.style.position = "relative";
-        schlangeElm.style.height = "100%";
-        schlangeElm.style.width = "100%";
-
-        neuerKopfElm.classList.add('Kopf');
-        neuerKopfElm.classList.add('Schlange');
-        neuerKopfElm.appendChild(schlangeElm);
-
-        let schwanzElms = document.getElementsByClassName('Schwanz');
-        let schwanzElm = schwanzElms.item(0)
-        schwanzElm.classList.remove('Schwanz');
-        schwanzElm.classList.remove('Schlange');
-        document.getElementById('Schwanz').remove();
-
-        let [y, x] = schwanzElm.id.split('_')
-        x = parseInt(x);
-        y = parseInt(y);
-
-        switch(richtung[(richtungLaenge) - (laenge - 1)]){
-            case 'W':
-                y--;
-                break;
-            case 'A':
-                x--;
-                break;
-            case 'S':
-                y++;
-                break;
             case 'D':
-                x++;
+                console.log(richtung[richtungLaenge - 1])
+                if(richtung[richtungLaenge - 1] == 'A'){
+                    neuerKopfElm = document.getElementById(`${y}_${x - 1}`);
+                    richtung.push('A');
+                    console.log(richtung);
+                    break;
+                }
+                neuerKopfElm = document.getElementById(`${y}_${x + 1}`);
+                richtung.push('D');
+                console.log(richtung);
                 break;
         }
+        if(x == 0 && richtung[richtungLaenge] == 'A' || x == 19 && richtung[richtungLaenge] == 'D' || y == 0 && richtung[richtungLaenge] == 'W' || y == 14 && richtung[richtungLaenge] == 'S'){
+            sterben();
+        }
+        if(neuerKopfElm.classList.contains('Apfel')){
+            laenge++;
 
-        let neuerSchwanzParent = document.getElementById(`${y}_${x}`);
-        let neuerSchwanzElm = neuerSchwanzParent.firstChild;
-        neuerSchwanzParent.classList.add('Schwanz');
-        neuerKopfElm.classList.add('Schlange');
-        neuerSchwanzElm.id = 'Schwanz';
+            if(laenge == 300){
+                alert('win');
+                location.reload;
+            }
+
+            schlangeElm = document.createElement('div');
+            schlangeElm.id = 'Kopf';
+            schlangeElm.style.position = "relative";
+            schlangeElm.style.height = "100%";
+            schlangeElm.style.width = "100%";
+            
+            neuerKopfElm.removeChild(neuerKopfElm.firstChild);
+            neuerKopfElm.classList.add('Kopf');
+            neuerKopfElm.classList.add('Schlange');
+            neuerKopfElm.classList.remove('Apfel');
+            if(laenge < 301 - aepfel){
+                randomApfel();
+            }
+            neuerKopfElm.appendChild(schlangeElm);
+        }else if(neuerKopfElm.classList.contains('Schlange') && !(neuerKopfElm.classList.contains('Schwanz'))){
+            sterben();
+        }else{
+            schlangeElm = document.createElement('div');
+            schlangeElm.id = 'Kopf';
+            schlangeElm.style.position = "relative";
+            schlangeElm.style.height = "100%";
+            schlangeElm.style.width = "100%";
+
+            neuerKopfElm.classList.add('Kopf');
+            neuerKopfElm.classList.add('Schlange');
+            neuerKopfElm.appendChild(schlangeElm);
+
+            let schwanzElms = document.getElementsByClassName('Schwanz');
+            let schwanzElm = schwanzElms.item(0)
+            schwanzElm.classList.remove('Schwanz');
+            schwanzElm.classList.remove('Schlange');
+            document.getElementById('Schwanz').remove();
+
+            let [y, x] = schwanzElm.id.split('_')
+            x = parseInt(x);
+            y = parseInt(y);
+
+            switch(richtung[(richtungLaenge) - (laenge - 1)]){
+                case 'W':
+                    y--;
+                    break;
+                case 'A':
+                    x--;
+                    break;
+                case 'S':
+                    y++;
+                    break;
+                case 'D':
+                    x++;
+                    break;
+            }
+
+            let neuerSchwanzParent = document.getElementById(`${y}_${x}`);
+            let neuerSchwanzElm = neuerSchwanzParent.firstChild;
+            neuerSchwanzParent.classList.add('Schwanz');
+            neuerKopfElm.classList.add('Schlange');
+            neuerSchwanzElm.id = 'Schwanz';
+        }
     }
-}
 
-setInterval(bewegen, 520);
+    setInterval(bewegen, 520);
 
-function sterben(){
-    alert('Ded');
-    location.reload();
+    function sterben(){
+        alert('Ded');
+        location.reload();
+    }
 }
---
+++ Snake/spiel.php
@@ -0,0 +1,32 @@
+<? $time = time() ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="./styles/stylesIndex.css?v=<? echo $time; ?>">
+        <script src="scripts/script.js?v=<? echo $time; ?>" defer></script>
+        <?
+            echo("
+                <script>
+                    window.addEventListener('load', function () {
+                        script($aepfel);
+                    });
+                </script>
+            ")
+        ?>
+    </head>
+
+    <body>
+        <div id="Spielfeld">
+            <?php
+                for($i = 0; $i < 15; $i++){
+                    for($j = 0; $j < 20; $j++){
+                        echo "<div id='$i" . "_$j'></div>";
+                    }
+                }
+            ?>
+        </div>
+    </body>
+</html>
