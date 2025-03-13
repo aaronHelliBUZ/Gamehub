04b4e1fdff7f361af0c85c11aae6e18962ddbb55
Nico245295 <nico.reisacher@buhlergroup.com>
2025-03-13 14:50:10 +0100

    Blockblast Blöcke platzieren und sie bleiben

--- Blockblast/html/index.php
+++ Blockblast/html/index.php
@@ -1,15 +1,19 @@
 <? $time = time() ?>
 <!DOCTYPE html>
 <html lang="de">
-   <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Blockblast</title>
-      <link rel="stylesheet" href="../css/styles.css?v=<? echo $time; ?>"/>
-      <script src="../js/JavaScript.js?v=<? echo $time; ?>" defer></script>
-      <link rel="icon" href="../gfx/icon.jpg" />
-   </head>
-   <body id="body">
-         <div id="block" onmousedown="bewegen(this)"></div>
-   </body>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Blockblast</title>
+        <link rel="stylesheet" href="../css/styles.css?v=<? echo $time; ?>" />
+        <script src="../js/JavaScript.js?v=<? echo $time; ?>" defer></script>
+        <link rel="icon" href="../gfx/icon.jpg" />
+    </head>
+    <body>
+        <div id="content">
+            <div id="gameField"></div>
+            <br /><br />
+            <div id="blockChoice"></div>
+        </div>
+    </body>
 </html>
--- Blockblast/js/JavaScript.js
+++ Blockblast/js/JavaScript.js
@@ -1,8 +1,35 @@
 //Häufig benutzte Elemente
-//const gameField = document.getElementById("gameField");
-//const content = document.getElementById("content")
+const gameField = document.getElementById("gameField");
+const blockChoiceElm = document.getElementById("blockChoice");
 
+const fieldsPerX = 10;
+const fieldsPerY = 10;
 
+let mouseX = -3;
+let mouseY = -3;
+
+let oldMouseX = -3;
+let oldMouseY = -3;
+
+let blockIdCounter = 0;
+
+let mouseDown = false;
+
+generateField();
+
+for (let i = 0; i < 3; i++) {
+    generateBlockInblockChoiceElm();
+}
+
+document.addEventListener("mouseup", function (event) {
+    event.preventDefault();
+    mouseDown = false;
+    oldMouseX = -3;
+    oldMouseY = -3;
+
+    let mouseElm = document.getElementById("X" + mouseX + "Y" + mouseY);
+    mouseElm.classList.add("setted");
+});
 
 //Zentriert das Speilfeld und stellt die grösse ein
 /*{
@@ -16,30 +43,72 @@
 }*/
 
 //Felder erstellen
-/*{
-    for (let x = 0; x < 8; x++){
-        for (let y = 0; y < 8; y++) {
-            let field = document.createElement("div");
+function generateField() {
+    console.log("Generate Fields");
 
-            field.id = x+"-"+y;
-            field.className = "";
-            gameField.appendChild(field);
+    for (let x = 0; x < fieldsPerX; x++) {
+        let newFlex = document.createElement("div");
+        newFlex.id = "X" + x.toString();
+        newFlex.className = "fieldFlex";
+        gameField.appendChild(newFlex);
+
+        for (let y = 0; y < fieldsPerY; y++) {
+            let newField = document.createElement("div");
+            newField.id = "X" + x.toString() + "Y" + y.toString();
+            newField.addEventListener("mouseover", function (event) {
+                event.preventDefault();
+                mouseOver(this);
+            });
+            newField.className = "field";
+            newFlex.appendChild(newField);
         }
     }
-}*/
+}
 
-let mouseX;
-let mouseY;
-
-document.addEventListener('mousemove', function(event){
+/*document.addEventListener("mousemove", function (event) {
     mouseX = event.clientX;
     mouseY = event.clientY;
-    console.log(mouseX + '_' + mouseY)
-})
+    console.log("X: " + mouseX + " Y: " + mouseY);
+});*/
 
-function bewegen(event){
-    var rect = event.getBoundingClientRect();
+function mouseClicked() {
+    mouseDown = true;
+    console.log("MOUSECLICKED");
+    console.log("OldX: " + oldMouseX + "\nOldY: " + oldMouseY);
+}
 
-    rect.left = mouseX;
-    rect.top = mouseY;
+function generateBlockInblockChoiceElm() {
+    let newBlock = document.createElement("div");
+    newBlock.id = "B" + blockIdCounter;
+    newBlock.classList.add("block");
+    blockChoiceElm.appendChild(newBlock);
+    blockIdCounter++;
+
+    newBlock.addEventListener("mousedown", function (event) {
+        event.preventDefault();
+        mouseClicked();
+    });
+}
+
+function mouseOver(mouseOverElm) {
+    if (mouseDown === true) {
+        oldMouseX = mouseX;
+        oldMouseY = mouseY;
+
+        mouseX = mouseOverElm.id.split("Y")[0].split("X")[1];
+        mouseY = mouseOverElm.id.split("Y")[1];
+        console.log("X: " + mouseX + "\nY: " + mouseY);
+        setFieldToBlockWhileMouseOver();
+    }
+}
+
+function setFieldToBlockWhileMouseOver() {
+    console.log("OldX2: " + oldMouseX + "\nOldY2: " + oldMouseY);
+    let oldBlockElm = document.getElementById("X" + oldMouseX + "Y" + oldMouseY);
+    if (oldBlockElm.classList.contains("setted") === false) {
+        oldBlockElm.classList.remove("fieldBlock");
+    }
+
+    let newBlockElm = document.getElementById("X" + mouseX + "Y" + mouseY);
+    newBlockElm.classList.add("fieldBlock");
 }
