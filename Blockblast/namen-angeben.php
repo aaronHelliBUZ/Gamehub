ccd3855e837b5842f60edfa18fdf13bed8a4660f
Nico245295 <nico.reisacher@buhlergroup.com>
2025-03-21 15:05:57 +0100

    bessere Styles

--- Blockblast/namen-angeben.php
+++ Blockblast/namen-angeben.php
@@ -9,7 +9,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Blockblast</title>
-        <link rel="stylesheet" href="css/styleName.css?v=<? echo $time; ?>">
+        <link rel="stylesheet" href="css/stylesName.css?v=<? echo $time; ?>">
         <link rel="icon" href="../gfx/!!!!@NICOLA!!!!.jpg"><!-- <============================= -->
     </head>
     <body>
